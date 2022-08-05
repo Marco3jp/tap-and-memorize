a29f0a634938fef5b5d8833926145b5fbe4b6494
marco <marco3jp+git@gmail.com>
2022-08-06 02:47:30 +0900

    デプロイできるようにした

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.DEPLOY_ENV === "GH_PAGES" ? "/tap-and-memorize/" : "/",
   plugins: [react()]
 })
